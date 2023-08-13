6ac1dd23713e01f1d50c0a673bd112f5b0be275f
Eduardo Diniz <eduardogomesdiniz42@gmail.com>
2023-08-13 20:22:37 -0300

    wip

--- src/screens/employees/cadastro/Cadastro.js
+++ src/screens/employees/cadastro/Cadastro.js
@@ -1,76 +1,79 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, SafeAreaView } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import ButtonC from '../../../components/button/ButtonC';
+import { useNavigation } from '@react-navigation/native';
 
 const Cadastro = () => {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [cpf, setCPF] = useState('');
   const [codigoEmpresa, setCodigoEmpresa] = useState('');
+  const navigation = useNavigation();
 
   const handleSubmit = () => {
-    console.log('Dados enviados:', {
-      nome,
-      email,
-      cpf,
-      codigoEmpresa,
-    });
+    navigation.navigate('Profile');
+
+  
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Criar Perfil</Text>
-      <View style={styles.form}>
-        <Text style={styles.label}>Seu nome:</Text>
-        <TextInput
-          style={styles.input}
-          value={nome}
-          onChangeText={setNome}
-        />
+    <SafeAreaView style={styles.container}>
+      <KeyboardAwareScrollView contentContainerStyle={styles.scrollContainer}>
+        <Text style={styles.title}>Criar Perfil</Text>
+        <View style={styles.form}>
+          <Text style={styles.label}>Seu nome:</Text>
+          <TextInput
+            style={styles.input}
+            value={nome}
+            onChangeText={setNome}
+          />
 
-        <Text style={styles.label}>Email:</Text>
-        <TextInput
-          style={styles.input}
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-        />
+          <Text style={styles.label}>Email:</Text>
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+          />
 
-        <Text style={styles.label}>CPF:</Text>
-        <TextInput
-          style={styles.input}
-          value={cpf}
-          onChangeText={setCPF}
-          keyboardType="numeric"
-        />
+          <Text style={styles.label}>CPF:</Text>
+          <TextInput
+            style={styles.input}
+            value={cpf}
+            onChangeText={setCPF}
+            keyboardType="numeric"
+          />
 
-        <Text style={styles.label}>Código da empresa:</Text>
-        <TextInput
-          style={styles.input}
-          value={codigoEmpresa}
-          onChangeText={setCodigoEmpresa}
-        />
-
-      </View>
-      <ButtonC style={styles.button} name="Enviar" onPress={handleSubmit} />
-    </View>
+          <Text style={styles.label}>Código da empresa:</Text>
+          <TextInput
+            style={styles.input}
+            value={codigoEmpresa}
+            onChangeText={setCodigoEmpresa}
+          />
+        </View>
+        <ButtonC style={styles.button} name="Enviar" onPress={handleSubmit} />
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: 'white',
   },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   button: {
-    padding: 10,
+    padding: 35,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
   },
   form: {
     width: '80%',
